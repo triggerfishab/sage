e39ed84a05dc6a9e5476cb564e0ab97d58361a0d
Joi Glifberg <joi.glifberg@gmail.com>
2019-09-03 09:35:34 +0200

    Add dynamic page components field group imports.

--- app/includes/acf-field-groups/page-components.php
+++ app/includes/acf-field-groups/page-components.php
@@ -6,11 +6,34 @@ $fields = [
         'label' => esc_html__('Page Components', 'sage'),
         'button_label' => esc_html__('Add Component', 'sage'),
         'type' => 'flexible_content',
-        'layouts' => [
-
-        ],
+        'layouts' => [],
     ],
 ];
+
+// Use opendir() instead of glob() because of the huge performance gain.
+$handle = opendir(__DIR__ . '/page-components/');
+
+if ($handle) {
+    while (false !== ($file_name = readdir($handle))) {
+        if ('.' === $file_name || '..' === $file_name) {
+            continue;
+        }
+
+        $component_config = include_once __DIR__ . '/page-components/' . $file_name;
+
+        if (empty($component_config)) {
+            continue;
+        }
+
+        $fields[0]['layouts'][] = $component_config;
+    }
+
+    if (isset($fields[0]['layouts'])) {
+        asort($fields[0]['layouts']);
+    }
+
+    closedir($handle);
+}
 
 acf_add_local_field_group([
     'key' => 'group_page_components',
